688a9fa8c5aaa30e053183f460893b44acfc49be
Olivier RICARD <olivier@blogspirit.fr>
2013-02-07 15:04:41 +0100

    init repo

---
+++ src/Tws/Common/TwsClient.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tws\Common;
+
+use Guzzle\Common\Collection;
+use Guzzle\Service\Client;
+use Guzzle\Service\Description\ServiceDescription;
+use Tws\Plugin\Auth\AuthPlugin;
+
+/**
+ * @see http://guzzlephp.org/tour/building_services.html
+ * @see https://github.com/klaussilveira/phpcs-psr
+ */
+class TwsClient extends Client {
+    /**
+     * Factory method to create a new TwsClient
+     *
+     * @param array|Collection $config Configuration data
+     *
+     * @return self
+     */
+    public static function factory($config = array()) {
+        $default = array(
+            'base_url' => $config['api_url']
+        );
+        $required = array('consumer_key', 'base_url');
+        $config = Collection::fromConfig($config, $default, $required);
+
+        $client = new self($config->get('base_url'), $config);
+        // Attach a service description to the client
+        $description = ServiceDescription::factory(__DIR__ . '/../Resources/service.json');
+        $client->setDescription($description);
+        $client->addSubscriber(new AuthPlugin($config));
+        return $client;
+    }
+}
---
+++ src/Tws/Plugin/Auth/AuthPlugin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tws\Plugin\Auth;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Guzzle\Common\Event;
+
+/**
+ *
+ */
+class AuthPlugin implements EventSubscriberInterface {
+    private $config;
+
+    function __construct($_config)
+    {
+        $this->config = $_config;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array('request.before_send' => 'onRequestBeforeSend');
+    }
+
+    public function onRequestBeforeSend(Event $event)
+    {
+        $event['request']->setHeader('Consumer-Key', $this->config['consumer_key']);
+        $event['request']->setheader('Content-Type', 'application/json');
+        $event['request']->setheader('Accept', 'application/json');
+
+        if(isset($this->config['token'])) {
+            $this->addAuthorizationHeader($event);
+        }
+    }
+
+    private function addAuthorizationHeader(Event $event) {
+        $auth_attributes = array();
+        $auth_attributes['token'] = $this->config['token'];
+        $auth_attributes['class'] = 'oauth';
+        $auth_attributes['method'] = 'hmac-sha-1';
+        $auth_attributes['timestamp'] = time();
+        $auth_attributes['nonce'] = md5(uniqid(rand(), true));
+        $str = '';
+        $str2 = '';
+        foreach($auth_attributes as $k => $v) {
+            $str .= "$k=\"$v\",";
+        }
+        ksort($auth_attributes);
+        foreach($auth_attributes as $k => $v) {
+            $str2 .= "$k=$v,";
+        }
+        $path = $event['request']->getPath();
+        $query = $event['request']->getQuery();
+        $method = $event['request']->getMethod();
+        $host = $event['request']->getHost();
+        $digest = "$method," . $host . ':80,' . $str2 . $this->config['consumer_secret'] . ',' . $path. $query;
+        $auth = sha1($digest);
+        $event['request']->setHeader("Authorization", "Token $str auth=\"$auth\"");
+    }
+}
