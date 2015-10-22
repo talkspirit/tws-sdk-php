ef80ddc2ea75086a766fec2b3f625f7f8a0ba399
Charles Bayle <charles.bayle@talkspirit.com>
2015-10-22 11:30:36 +0200

    Remove TwsRequest extended class, not used for authentification (POST)

--- src/Tws/Common/TwsRequestFactory.php
+++ src/Tws/Common/TwsRequestFactory.php
@@ -8,7 +8,6 @@ use Guzzle\Http\Url;
 use Guzzle\Http\Message\RequestFactory;
 use Guzzle\Parser\ParserRegistry;
 use Tws\Common\TwsEntityEnclosingRequest;
-use Tws\Common\TwsRequest;
 
 /**
  * Default HTTP request factory used to create the default {@see Request} and {@see EntityEnclosingRequest} objects.
@@ -17,13 +16,8 @@ class TwsRequestFactory extends RequestFactory
 {
     /**
      * extends requestClass to customize Guzzle\Http\Message\Request::setAuth()
-     * Guzzle\Http\Message\RequestFactory::create() send two types of request Object (Request & EntityEnclosingRequest)
-     */
-    protected $requestClass = 'Tws\\Common\\TwsRequest';
-
-    /**
-     * extends requestClass to customize Guzzle\Http\Message\Request::setAuth()
-     * Guzzle\Http\Message\RequestFactory::create() send two types of request Object (Request & EntityEnclosingRequest)
+     * Guzzle\Http\Message\RequestFactory::create() send two types of request Object : Request & EntityEnclosingRequest
+     * Request object is defined for GET, authenfication is only in POST => no need to extend Request
      */
     protected $entityEnclosingRequestClass = 'Tws\\Common\\TwsEntityEnclosingRequest';
 }
